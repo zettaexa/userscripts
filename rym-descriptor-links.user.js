864ae3ee0523758ed5d2d5fb06f4ad301dd4c6e8
zetta <79076739+zettaexa@users.noreply.github.com>
2025-11-21 21:32:07 -0700

    Add MIT license to user script metadata
    Add license information to the user script header.

--- rym-descriptor-links.user.js
+++ rym-descriptor-links.user.js
@@ -7,6 +7,7 @@
 // @match        https://rateyourmusic.com/release/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rateyourmusic.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
